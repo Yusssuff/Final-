6eb814d565ce0e346a4895ba45fc9ccfae34b51e
Yusssuff <yussuf.has2005@gmail.com>
2026-08-19 19:44:20 +0300

    making the data

---
+++ Data/Product.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Final_Task.Data
+{
+    public class Product
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [Range(0, double.MaxValue)]
+        public decimal Price { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int Quantity { get; set; }
+    }
+}
+
+    }
+}
